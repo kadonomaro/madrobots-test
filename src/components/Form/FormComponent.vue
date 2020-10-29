1780ed7d73233e6a77a3f66074bcea278fb4a86d
Kadonomaro <denis.afer@gmail.com>
2020-10-29 22:41:31 +0700

    feat: add FormComponent template

---
+++ src/components/Form/FormComponent.vue
@@ -0,0 +1,83 @@
+<template>
+	<form :action="action" class="form" @submit.prevent="submitHandler">
+		<label class="form__label">
+			<input
+				type="text"
+				class="input form__field"
+				placeholder="Имя"
+				v-model="form.name"
+			>
+		</label>
+		<label class="form__label">
+			<input
+				type="email"
+				class="input form__field"
+				placeholder="Почта"
+				v-model="form.email"
+			>
+		</label>
+		<label class="form__label">
+			<input
+				type="tel"
+				class="input form__field"
+				placeholder="Телефон"
+				v-model="form.phone"
+			>
+		</label>
+		<label class="form__label">
+			<textarea
+				rows="5"
+				class="input form__field form__field--type--textarea"
+				placeholder="Комментарий"
+				v-model="form.comment"
+			></textarea>
+		</label>
+		<div class="form__controls">
+			<button class="button form__button" type="submit">Отправить</button>
+		</div>
+	</form>
+</template>
+
+<script>
+export default {
+	name: 'FormComponent',
+	props: {
+		action: {
+			type: String,
+			required: true
+		}
+	},
+	data() {
+		return {
+			form: {
+				name: '',
+				email: '',
+				phone: '',
+				comment: ''
+			}
+		}
+	},
+	methods: {
+		submitHandler() {
+			console.log(this.form);
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+	.form {
+		&__label {
+			display: block;
+			&:not(:last-child) {
+				margin-bottom: 10px;
+			}
+		}
+		&__field {
+			width: 100%;
+		}
+		&__field--type--textarea {
+			min-height: 100px;
+		}
+	}
+</style>
--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,12 +1,19 @@
 <template>
 	<div class="page">
 		<h1 class="page__title">Валидация формы</h1>
+		<div class="page__component">
+			<FormComponent :action="'/'" />
+		</div>
 	</div>
 </template>
 
 <script>
+import FormComponent from '@/components/Form/FormComponent';
 
 export default {
-	name: 'FormPage'
+	name: 'FormPage',
+	components: {
+		FormComponent
+	}
 }
 </script>
